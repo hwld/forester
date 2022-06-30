b8622a1917cb202eee8bf6ace96e5d584bcca141
hwld <0701hw@gmail.com>
2022-06-30 22:36:58 +0900

    Add useDisclosure

---
+++ app/hooks/useDisclosure.tsx
@@ -0,0 +1,19 @@
+import { useCallback, useState } from "react";
+
+export const useDisclosure = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const open = useCallback(() => {
+    setIsOpen(true);
+  }, []);
+
+  const close = useCallback(() => {
+    setIsOpen(false);
+  }, []);
+
+  const toggle = useCallback(() => {
+    setIsOpen((s) => !s);
+  }, []);
+
+  return { isOpen, open, close, toggle };
+};
